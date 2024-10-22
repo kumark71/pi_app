e4256f9f52d29e1c4a8510a6c3da6d9e9662ed42
kumark71 <kumar.kharare@healthledger.co.in>
2024-10-22 12:43:50 +0530

    splash screen image changed

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -12,19 +12,13 @@ class SplashScreen extends StatelessWidget {
     final double screenWidth = MediaQuery.of(context).size.width;
     final double screenHeight = MediaQuery.of(context).size.height;
 
-    final double svgSize = screenWidth * 0.5; // 50% of the screen width
-
     return Scaffold(
       backgroundColor: Colors.white, // Adjust the background color if needed
       body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(
-              screenWidth * 0.1), // 10% padding based on screen width
-          child: SvgPicture.asset(
-            'assets/images/smartqr_logo.svg',
-            width: svgSize, // Responsive width
-            height: svgSize, // Responsive height
-          ),
+        child: Image.asset(
+          'assets/images/smartqr_splash_image.png',
+          width: screenWidth,
+          height: screenHeight,
         ),
       ),
     );
